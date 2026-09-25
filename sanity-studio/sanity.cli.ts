0ff9e42232b21fa718aad4ee7e8dcf4405ad7b89
aaronalayo <aaron.aa@me.com>
2026-09-25 14:33:05 +0200

    Add Sanity Studio for artwork

---
+++ sanity-studio/sanity.cli.ts
@@ -0,0 +1,15 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'cdjc7mii',
+    dataset: 'production'
+  },
+  deployment: {
+    /**
+     * Enable auto-updates for studios.
+     * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
+     */
+    autoUpdates: true,
+  },
+})
